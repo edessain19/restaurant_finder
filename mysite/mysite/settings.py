da60d14258e17b9462e054f03ceb37d3bdf5d755
Evrard Dessain <edessain@fu-r2-p5.s19.be>
2021-08-09 15:47:10 +0200

    modified settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # environ.Env.read_env()
 
 API_KEY = os.getenv('APY_KEY')
+django_heroku.settings(locals())
